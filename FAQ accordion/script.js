369bdafab77102792e58e648c95f5a2be11b3bd5
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-03-05 20:14:18 -0300

    full application

--- FAQ accordion/script.js
+++ FAQ accordion/script.js
@@ -1,1 +1,24 @@
 const plus = document.querySelectorAll('plus');
+const minus = document.querySelectorAll('minus-hidden');
+const para = document.querySelectorAll('para-hidden');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const plus = document.querySelectorAll('.plus');
+    const minus = document.querySelectorAll('.minus-hidden');
+    const para = document.querySelectorAll('.para-hidden');
+
+    for (let i = 0; i < plus.length; i++) {
+        plus[i].addEventListener('click', function() {
+            plus[i].classList.toggle("hidden");
+            minus[i].classList.toggle("hidden");
+            para[i].classList.toggle("hidden");
+        });
+
+        minus[i].addEventListener('click', function() {
+            plus[i].classList.toggle("hidden");
+            minus[i].classList.toggle("hidden");
+            para[i].classList.toggle("hidden");
+        });
+    }
+});
