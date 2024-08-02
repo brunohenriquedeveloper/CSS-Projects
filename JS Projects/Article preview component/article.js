ab238140a5e9f9a2d4d69a1d92b0a3dbcf90a0f8
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-08-01 22:21:09 -0300

    full application

--- JS Projects/Article preview component/article.js
+++ JS Projects/Article preview component/article.js
@@ -1,5 +1,13 @@
-const share = document.querySelector(".interactive")
-
-interactive.addEventListener('mouseover', () => {
-    
+const inte = document.querySelector('.share-btn').addEventListener('click', function() {
+    document.querySelector(".share").style.display ='flex'
+    document.getElementById("btn-close").style.display = 'inline-block'
+    document.getElementById("btn").style.display = "none"
 })
+
+function init(){
+    document.querySelector(".share").style.display = "none"
+    document.getElementById("btn-close").style.display = 'inline-block'
+    document.getElementById("btn").style.display = "none"
+}
+
+document.getElementById("btn-close").addEventListener('click', init)
